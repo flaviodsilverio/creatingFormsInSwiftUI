339b337c75324b375866c17847634108da3bd2f7
Flávio Silvério <flaviosilverio@Flavios-MacBook-Pro.local>
2023-11-20 20:30:34 +0100

    first commit

---
+++ Creating Forms in SwiftUI/Creating_Forms_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Creating_Forms_in_SwiftUIApp.swift
+//  Creating Forms in SwiftUI
+//
+//  Created by Flávio Silvério on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Creating_Forms_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
